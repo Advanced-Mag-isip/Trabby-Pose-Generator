f08115be69825d1f346beda49bb9b1d2c46eea46
Kenneth Montes <kamontes@up.edu.ph>
2026-05-29 14:12:51 +0800

    added access

--- TrabbyPose/frontend/astro.config.mjs
+++ TrabbyPose/frontend/astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     server: {
         host: '0.0.0.0',
-        port: 8312
+        port: 8312,
+        allowedHosts: ['sandbox1.advancedthinkers.app']
     }
 });
