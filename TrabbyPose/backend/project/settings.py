6c923a3c255786a732783cf5643bb7c9076a1601
Kenneth Montes <kamontes@up.edu.ph>
2026-05-31 11:46:53 +0800

    changes in .yml

--- TrabbyPose/backend/project/settings.py
+++ TrabbyPose/backend/project/settings.py
@@ -130,5 +130,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:8312", 
+    "http://localhost:4322",
+    "http://127.0.0.1",
+    "http://localhost"
 
 ]
